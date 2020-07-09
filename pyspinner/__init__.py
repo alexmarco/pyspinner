40661a101e1ae469f6c23b2b05bd0c7817330200
MARCO RAMOS ALEJANDRO <x383138@es.isban.com>
2020-07-09 22:07:00 +0200

    - Added test - Rename `pyspinner.py` to `_core.py` - Change class `CapturingSysOutput`from __init__.py to _utils.py

--- pyspinner/__init__.py
+++ pyspinner/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for Python terminal spinner."""
 
 __author__ = """Alejandro Marco Ramos"""
-__email__ = 'alejandro.marco.ramos@gmail.com'
-__version__ = '0.1.0'
+__email__ = "alejandro.marco.ramos@gmail.com"
+__version__ = "0.1.0"
+
+from ._core import Spinner  # noqa
