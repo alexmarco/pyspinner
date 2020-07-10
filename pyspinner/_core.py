f8f861ce160fa9a141cbf7990b4e2c999f229429
MARCO RAMOS ALEJANDRO <x383138@es.isban.com>
2020-07-10 08:57:48 +0200

    - Add SpinnerError class Exception - Add attributes setter - Change update logic - Change thread start stop logic

--- pyspinner/_core.py
+++ pyspinner/_core.py
@@ -7,6 +7,10 @@ import itertools
 from functools import partial
 
 
+class SpinnerError(Exception):
+    pass
+
+
 class Spinner:
 
     _default_busy = False
@@ -21,26 +25,50 @@ class Spinner:
         show_progress=None,
         progress_chars=None,
         delimiter=None,
-        units_text=None,
+        units=None,
     ):
         """"""
         self.init_text = init_text or "Working..."
-        self.delay = delay or self._default_delay
+        self.show_delay = delay or self._default_delay
         self.spinner_generator = itertools.cycle(progress_chars or self._default_chars)
         self.delimiter = delimiter or self._default_delimiter
-        self.show_progress = show_progress
+        self._units = units or "items"
         self._updated_value = 0
         self._updated_tmpl = ""
-        self._units_text = units_text or "items"
+        self._thread = None
+        self.show_progress = show_progress
 
-    def update(self, value):
+    @property
+    def show_delay(self):
+        return self._show_delay
+
+    @show_delay.setter
+    def show_delay(self, value):
         """"""
+        if not isinstance(value, (int, float)):
+            raise SpinnerError(
+                "`show_delay` value must be numeric, given %s" % type(value)
+            )
+        self._show_delay = value
+
+    @property
+    def show_progress(self):
+        return self._show_progress
+
+    @show_progress.setter
+    def show_progress(self, value):
+        """"""
+        self._show_progress = value
+        if value:
+            self._updated_tmpl = "(processed {self._updated_value} %s" % self._units
+
+    def update(self, value=None):
+        """"""
+        if not self._thread or not self._thread.is_alive():
+            raise SpinnerError("Updatting value with no alive threads.")
         try:
-            self._updated_value += value
-            if self.show_progress:
-                self._updated_tmpl = (
-                    f"(processed {self._updated_value} {self._units_text})"
-                )
+            self._updated_value += value or 1
+            self._progress_text = f"{self._updated_tmpl}"
         except TypeError as exc:
             if "unsupported operand" in repr(exc):
                 raise TypeError("`value` must be numeric, given %s" % type(value))
@@ -50,17 +78,39 @@ class Spinner:
         """"""
         p_print = partial(print, sep=self.delimiter, end="\r", flush=True)
 
-        while self.busy:
+        while self._busy:
             p_print(self.init_text, self._updated_tmpl, next(self.spinner_generator))
-            time.sleep(self.delay)
+            time.sleep(self._show_delay)
+
+    @property
+    def busy(self):
+        return self._busy
+
+    @property
+    def thread(self):
+        return self._thread
+
+    def start(self):
+        """"""
+        if self._thread and self._thread.is_alive():
+            raise SpinnerError("Exists alive spinner thread, stop first.")
+
+        self._busy = True
+        self._thread = threading.Thread(target=self.spinner_task)
+        self._thread.start()
+        return self._thread
+
+    def stop(self):
+        """"""
+        if self._thread and self._thread.is_alive():
+            self._busy = False
+            self._thread.join(timeout=self._show_delay * 2)
 
     def __enter__(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
+        self.start()
         return self
 
     def __exit__(self, exception, value, tb):
-        self.busy = False
-        time.sleep(self.delay)
+        self.stop()
         if exception is not None:
             return False
--- tests/test_pyspinner.py
+++ tests/test_pyspinner.py
@@ -7,6 +7,7 @@ import unittest
 # from collections.abc import Generator
 
 from pyspinner import Spinner
+from pyspinner._core import SpinnerError
 
 from _utils import CapturingSysOutput
 
@@ -25,6 +26,12 @@ class TestPyspinner(unittest.TestCase):
         """Test something."""
         spi = Spinner()
         self.assertEqual(spi.init_text, "Working...")
+        self.assertEqual(spi._updated_tmpl, "")
+
+        spi.show_progress = True
+        self.assertEqual(spi._updated_tmpl, "(processed {self._updated_value} items")
+        with self.assertRaises(SpinnerError):
+            spi.update()
 
     def test_run_01(self):
         """"""
@@ -54,7 +61,21 @@ class TestPyspinner(unittest.TestCase):
         with CapturingSysOutput() as out:
             with Spinner(show_progress=True) as spi:
                 for x in range(10):
-                    spi.update(1)
+                    spi.update()
                     time.sleep(0.2)
         for line in out.result.splitlines():
             self.assertRegex(line, r"Working...\s+(\(processed \d+ items\))?\W+")
+
+    def test_run_04(self):
+        """"""
+        spi = Spinner()
+        self.assertEqual(spi._thread, None)
+        spi.start()
+        time.sleep(0.1)
+        self.assertEqual(spi._thread.is_alive(), True)
+        spi.stop()
+        self.assertEqual(spi._thread.is_alive(), False)
+        spi.start()
+        with self.assertRaises(SpinnerError):
+            spi.start()
+        spi.stop()
